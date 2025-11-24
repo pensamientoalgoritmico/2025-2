9aafa281a0457dd2024123f26b466a7e8e041bed
Adelaida Avila <adelaidaavila.c@gmail.com>
2025-11-24 19:05:41 +0100

    carga entregas

---
+++ estudiantes/pineda-juan-sebastian/sketch.js
@@ -0,0 +1,23 @@
+//Plantilla para la entrega final
+//Nombre
+// Nombre del proyecto
+//Descripción breve de lo que hace
+//links a referentes, breve explicación de para qué se usó la IA
+
+//Inicio código
+
+function setup() {
+  // NO EDITAR LA SIGUIENTE LINEA, HACE QUE SEA PANTALLA COMPLETA Y LO CENTRA EN WEB
+  createCanvas(windowWidth, windowHeight).parent("canvasContainer");
+  //sigue como siempre
+  background(255, 255, 255);
+}
+
+function draw() {
+  background(255, 255, 255);
+}
+
+//Ajustar el tamaño cuando cambia el tamaño de la pantalla
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
